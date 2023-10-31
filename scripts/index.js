0bb8ce200ec048d71c1c3e911f9930241ba68ab0
Jacqueline Oliveira <jacqueline.alterdata@gmail.com>
2023-10-30 23:13:21 -0300

    Projeto frontend para desafio

---
+++ scripts/index.js
@@ -0,0 +1,29 @@
+import getDados from "./getDados.js";
+
+const btnSortear = document.querySelector('.btn-sort');
+const fichaDescricao = document.getElementById('ficha-descricao');
+
+function carregarInfoSerie() {
+  getDados(`/series/frases`)
+      .then(data => {
+        fichaDescricao.innerHTML = `
+              <img src="${data.poster}" alt="${data.titulo}" />
+              <div>
+                  <h2>${data.titulo}</h2>
+                  <div class="descricao-texto">
+                      <p><i>"${data.frase}"</i></p>
+                      <p><b>Citado por:</b> ${data.personagem}</p>
+                  </div>
+              </div>
+          `;
+      })
+      .catch(error => {
+          console.error('Erro ao obter informações da série:', error);
+      });
+}
+
+
+window.onload = carregarInfoSerie();
+btnSortear.addEventListener('click', () => {
+    setTimeout(carregarInfoSerie, 500);
+});
